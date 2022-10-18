793b917c3b81096009b0754948c2b6db22fa85f2
dazcui <david.azcui@gmail.com>
2022-10-17 21:07:15 -0400

    segundo commit

--- src/users/users.services.js
+++ src/users/users.services.js
@@ -108,39 +108,35 @@ const getMyUser = (req, res) => {
 }
 
 const patchMyUser = (req, res) => {
-  const id = req.params.id;
-  const { firstName, lastName, phone, gender, country } = req.body;
+  const id = req.user.id;
+  const { firstName, lastName, phone, birthday, gender, country } = req.body;
 
   usersControllers
-    .updateUser(id, { firstName, lastName, phone, gender, country })
-    .then((data) => {
-      if (data[0]) {
-        res
-          .status(200)
-          .json({ message: `User with ID: ${id}, edited succesfully!` });
-      } else {
-        res.status(404).json({ message: "Invalid ID" });
-      }
+    .updateUser(id, { firstName, lastName, phone, birthday, gender, country })
+    .then(() => {
+      res.status(200).json({ message: `Your user was edited succesfully!` });
     })
     .catch((err) => {
       res.status(400).json({ message: err.message });
     });
 };
+
+//? 2 tipos de delete:
+//* 1. por administrador
+//* 2. por mi mismo
+
 const deleteMyUser = (req, res) => {
-  const id = req.params.id;
-  usersControllers
-    .deleteUser(id)
-    .then((data) => {
-      if (data) {
-        res.status(204).json();
-      } else {
-        res.status(404).json({ message: "Invalid ID" });
-      }
-    })
-    .catch((err) => {
-      res.status(400).json({ message: err.message });
-    });
+  const id = req.user.id;
+
+  usersControllers.updateUser(id, { status: "inactive" })
+      .then(() => {
+        res.status(200).json({ message: `Your user was deleted succesfully!` });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: err.message });
+      });
 };
+
 
 
 module.exports = {
