5b86ad73eedc33a655dbcf8434d04d9231b93812
dazcui <david.azcui@gmail.com>
2022-10-27 18:56:09 -0400

    add models

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,20 +1,8 @@
 const Users = require('./users.models')
-const Posts = require('./posts.models')
-const Categories = require('./categories.models')
 
-const initModels = ()=>{
+const initModels = () => {
 
-    //* 1 -> M
-    //? una publicacion pertenece a un usuario
-    Posts.belongsTo(Users)
-    //? un usuario tiene muchas publicaciones
-    Users.hasMany(Posts)
-
-
-    //* 1 -> M
-    //?una publicacion pertenece a una categoria
-    Posts.belongsTo(Categories)
-    //? una categoria tiene muchas publicaciones
-    Categories.hasMany(Posts)
 }
+
+
 module.exports = initModels
--- src/models/types.models.js
+++ src/models/types.models.js
@@ -2,22 +2,22 @@ const { DataTypes } = require('sequelize')
 
 const db = require('../utils/database')
 
-const Categories = db.define('categories', {
+const Types = db.define('types', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
         allowNull: false
-    }, 
+    },
     name: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull:false,
         unique: true
     }
 }, {
     //? Evita que sequelize cree la columna de createdAt y updatedAt
-    timestamps: false
+    timestamps: false,
+    
 })
 
-
-module.exports = Categories
+module.exports = Types
